35ed48840b53eb852112bd8f4e14a2757d92cef3
benmalartre <benmalartre@hotmail.com>
2021-06-29 16:54:25 +0200

    [STK] delete old C api backup

--- projects/CAPIBAK/STK/STK.build/Debug/STK.build/Objects-normal/x86_64/UdpSocket.d
+++
@@ -1,4 +0,0 @@
-dependencies: /Users/benmalartre/Documents/RnD/STK/src/UdpSocket.cpp \
-  /Users/benmalartre/Documents/RnD/STK/include/UdpSocket.h \
-  /Users/benmalartre/Documents/RnD/STK/include/Socket.h \
-  /Users/benmalartre/Documents/RnD/STK/include/Stk.h
